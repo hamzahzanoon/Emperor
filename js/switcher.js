9a9c91b63d6dd6718ff81bb4d7b17d4f90f20ab9
hamzahzanoon <aa6hamzah6aa@gmail.com>
2022-12-21 20:05:01 +0200

    helllo

--- js/switcher.js
+++ js/switcher.js
@@ -87,3 +87,19 @@ function changeColor(color) {
     });
     event.target.classList.add("active");
 }
+
+var firstIndex = 0;
+function automaticSlide() {
+    setTimeout(automaticSlide ,4000)
+    var pics; 
+    const img = document.querySelectorAll(".img-box");
+    for (let pics = 0; pics < img.length; pics++) {
+        img[pics].style.display = "none";
+    }
+    firstIndex++;
+    if(firstIndex > img.length) {
+        firstIndex = 1;
+    }
+    img[firstIndex -1].style.display = "block";
+}
+automaticSlide();
